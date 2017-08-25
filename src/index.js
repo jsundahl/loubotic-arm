25edfea86052ae765c314114ca1582226520e9b3
jsundahl <jsundahl@onshape.com>
2017-08-25 17:31:05 -0400

    consolidation didn't work and I need to do real work so I'm reverting

--- src/index.js
+++ src/index.js
@@ -21,27 +21,42 @@ app.ws('/', function(ws, req) {
     console.log(req.body);
     console.log('request made');
   });
-
-  postForKeys([
-    'w',
-    'a',
-    's',
-    'd',
-    'q',
-    'e',
-    'h'
-  ])
+  app.post('/w', function(req, res) {
+    console.log('w');
+    ws.send('w');
+    res.sendStatus(200)
+  });
+  app.post('/s', function(req, res) {
+    console.log('s');
+    ws.send('s');
+    res.sendStatus(200)
+  });
+  app.post('/a', function(req, res) {
+    console.log('a');
+    ws.send('a');
+    res.sendStatus(200)
+  });
+  app.post('/d', function(req, res) {
+    console.log('d');
+    ws.send('d');
+    res.sendStatus(200)
+  });
+  app.post('/q', function(req, res) {
+    console.log('q');
+    ws.send('q');
+    res.sendStatus(200)
+  });
+  app.post('/e', function(req, res) {
+    console.log('e');
+    ws.send('e');
+    res.sendStatus(200)
+  });
+  app.post('/h', function(req, res) {
+    console.log('h');
+    ws.send('h');
+    res.sendStatus(200)
+  });
 });
-
-function postForKeys(keys) {
-  keys.forEach((key) => {
-    app.post(`/${key}`, (req, res) => {
-      console.log(key);
-      ws.send(key);
-      res.sendStatus(200)
-    });
-  })
-}
 
 
 // Listen for requests
